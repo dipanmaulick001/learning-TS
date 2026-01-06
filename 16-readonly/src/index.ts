f9b95ff697eea3bfcebae102e81f071efdbbe0fe
Dipan <dipanmaulick25@gmail.com>
2026-01-06 19:27:45 +0000

    Readonly applications

---
+++ 16-readonly/src/index.ts
@@ -0,0 +1,17 @@
+//the idea of readonly : to prevent internals from being modified
+
+type User = {
+    name : string,
+    email : string,
+    age : number,
+    id : number
+}
+
+let user : Readonly<User> = {
+    name : "deep",
+    email : "deep@gmail.com",
+    age : 25,
+    id : 61200017965
+}
+
+user.age = 18 //cannot because it is readonly 
